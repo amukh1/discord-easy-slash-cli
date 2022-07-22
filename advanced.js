59fd7f9b56d390ccc40606c1ca8a0cf4e52152d4
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-07-22 17:00:14 -0400

    Made handler assess null function

--- advanced.js
+++ advanced.js
@@ -1,23 +1,33 @@
 let client = require('discord-easy-slash'); 
+let fs = require('fs');
 
-client.PUBLIC_KEY = '<PUBLIC_KEY>'
-client.TOKEN = '<TOKEN>'
-client.APP_ID = '<APPLICATION_ID>'
-client.GUILD_ID = 'GUILD_ID' // (you still need this, as you need to have a testing server)
+client.PUBLIC_KEY = '<PUBLIC_KEY>';
+client.TOKEN = '<TOKEN>';
+client.APP_ID = '<APPLICATION_ID>';
+client.GUILD_ID = 'GUILD_ID'; // (you still need this, as you need to have a testing server)
 
-client.command('test', 1, 'test', [ { "name": "number", "description": "number", "type": 4, "required": true, }, ])
-client.command('help', 1, 'help command', null)
+client.command('test', 1, 'test', [ { "name": "number", "description": "number", "type": 4, "required": true, }, ]);
+client.command('help', 1, 'help command', null);
 
 
 
 
 function handler(command, data, res, type){
-  console.log(`Command recived: ${command}`)
-    let runFile = require('./commands/' + command + '.js')
-    runFile(command, data, res, type)
-}
+  console.log(`Command recived: ${command}`);
+  fs.exists('./commands/' + command + '.js', function (exists) {
+    if(exists)
+    {
+      let runFile = require('./commands/' + command + '.js');
+      runFile(command, data, res, type);
+    }else
+    {
+       client.reply(res, '404, Function not found..', null);
+    };
+});
+   
+};
 
 client.on('slash-event', handler);
 
 
-client.listen(80)
+client.listen(80);
--- simple.js
+++ simple.js
@@ -1,20 +1,20 @@
 let client = require('discord-easy-slash'); 
 
-client.PUBLIC_KEY = '<PUBLIC_KEY>'
-client.TOKEN = '<TOKEN>'
-client.APP_ID = '<APPLICATION_ID>'
-client.GUILD_ID = 'GUILD_ID' // (you still need this, as you need to have a testing server)
+client.PUBLIC_KEY = '<PUBLIC_KEY>';
+client.TOKEN = '<TOKEN>';
+client.APP_ID = '<APPLICATION_ID>';
+client.GUILD_ID = 'GUILD_ID'; // (you still need this, as you need to have a testing server)
 
-client.command('t', 1, 'testingg', [ { "name": "numberrr", "description": "numberrrr", "type": 4, "required": true, }, ])
-client.command('help', 1, 'help command', null)
+client.command('test', 1, 'test', [ { "name": "number", "description": "number", "type": 4, "required": true, }, ]);
+client.command('help', 1, 'help command', null);
 
 
 
 
 function handler(command, data, res, type){
-  console.log(`Command recived: ${command}`)
+  console.log(`Command recived: ${command}`);
 if(command == 'help'){
-    client.reply(res, 'Help command?', null)
+    client.reply(res, 'Help command?', null);
   }else {
     let e = [
    {
@@ -41,12 +41,12 @@ if(command == 'help'){
                 },
                 "timestamp": new Date()
               },
-  ]
-     client.reply(res, 'Cound not find command..', e)
-  }
-}
+  ];
+     client.reply(res, 'Cound not find command..', e);
+  };
+};
 
 client.on('slash-event', handler);
 
 
-client.listen(80)
+client.listen(80);
